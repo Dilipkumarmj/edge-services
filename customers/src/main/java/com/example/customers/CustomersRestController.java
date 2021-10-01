fcf99abd5bb4ac11e929d55e6c62be1d59a6aa66
Dilipkumar <mail2Dilipu@gmail.com>
2021-10-01 19:57:50 +0530

    added customers application

---
+++ customers/src/main/java/com/example/customers/CustomersRestController.java
@@ -0,0 +1,28 @@
+package com.example.customers;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+@Controller
+@ResponseBody
+public class CustomersRestController {
+
+    private final AtomicInteger id = new AtomicInteger();
+
+    record Customer(@JsonProperty("id") Integer id,@JsonProperty("name") String name){}
+
+    private final List<Customer> customers = List.of("Mario", "Zhouyue", "Zhen", "Mia", "Stéphane", "Valerie", "Mike", "Julia")
+            .stream().map(name -> new Customer(this.id.incrementAndGet(),name)).collect(Collectors.toList());
+
+    @GetMapping("/customers")
+    Collection<Customer> get(){
+        return this.customers;
+    }
+}
